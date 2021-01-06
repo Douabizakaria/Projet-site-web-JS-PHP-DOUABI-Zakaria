216e9ba7c3ba1361ed56a51bdd9b637de22173f8
DOUABIZAKARIA <zakaria.douabi11@outlook.fr>
2021-01-06 23:13:20 +0100

    first commit

---
+++ infomerNous.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
+
+<head>
+
+	<meta charset="utf-8" /> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="robots" content="index, follow" />
+		<meta name="generator" content="RapidWeaver" />
+		 
+
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+
+	<title>informer Nous ! |  PROTEGEONS NOUS <em> AINSI QUE NOS PROCHE EN INFORMONS !</em></title>
+	<link rel="stylesheet" type="text/css" media="all" href="../rw_common/themes/Artful/consolidated.css" />
+		
+	    
+</head>
+
+
+<body>
+	<nav class="navbar navbar-expand-lg">
+		<div class="container">
+            <a class="navbar-brand" href="">
+                <div class="logo"><img src="../rw_common/images/Logo.png" width="200" height="200" alt=" PROTEGEONS NOUS <em> AINSI QUE NOS PROCHE EN INFORMONS !</em>"/></div>
+                <span class="navbar-title"> PROTEGEONS NOUS <em> AINSI QUE NOS PROCHE EN INFORMONS !</em></span>
+            </a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+			 aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav ml-auto"><li class="nav-item"><a href="../index.html" rel="" class="nav-link">Accueil</a></li><li class="nav-item active"><a href="infomerNous.php" rel="" class="nav-link">informer Nous !</a></li></ul>
+			</div>
+		</div>
+	</nav>
+	<div class="hero pb-5">
+		<div class="container">
+			<div class="row align-items-center justify-content-center">
+				<div class="col-sm-6 align-self-center">
+					<h1 class="hero-title mb-4">
+						 PROTEGEONS NOUS <em> AINSI QUE NOS PROCHE EN INFORMONS !</em>
+					</h1>
+					<p class="lead hero-slogan">
+						
+					</p>
+				</div>
+				<div class="col-sm-6">
+					<img src="../rw_common/images/téléchargement4.jpeg" width="100" alt="" class="banner_image">
+				</div>
+			</div>
+		</div>
+    </div>
+
+    <hr class="my-5">
+
+	<div class="container pt-5">
+        <div class="row intro justify-content-between">
+            <div class="col-sm-6">
+                
+<div class="message-text">Informer est notre Devoir </div><br />
+
+<form class="rw-contact-form" action="./files/mailer.php" method="post" enctype="multipart/form-data">
+	 <div>
+		<label>Nom & Prenom</label> *<br />
+		<input class="form-input-field" type="text" value="" name="form[element0]" size="40"/><br /><br />
+
+		<label>Portable</label> *<br />
+		<input class="form-input-field" type="text" value="" name="form[element1]" size="40"/><br /><br />
+
+		<label>Email</label> *<br />
+		<input class="form-input-field" type="text" value="" name="form[element2]" size="40"/><br /><br />
+
+		<label>object</label> *<br />
+		<input class="form-input-field" type="text" value="" name="form[element3]" size="40"/><br /><br />
+
+		<label>Message</label> *<br />
+		<textarea class="form-input-field" name="form[element4]" rows="8" cols="38"></textarea><br /><br />
+
+		<div style="display: none;">
+			<label>Spam Protection: Please don't fill this in:</label>
+			<textarea name="comment" rows="1" cols="1"></textarea>
+		</div>
+		<input type="hidden" name="form_token" value="9635886595fdc9d867da7e" />
+		
+		<input class="form-input-button" type="submit" name="submitButton" value="envoyer " />
+	</div>
+</form>
+
+<br />
+<div class="form-footer"> </div><br />
+
+
+            </div>
+            <div class="sidebar col-sm-6 order-md-8">
+                <h2 class="sidebar-title">
+                    
+                </h2>
+
+                 
+            </div>
+        </div>
+	</div>
+
+	<div class="footer">
+		<div class="container">
+			<div class="row justify-content-start">
+				<div class="col">
+					<div class="footer-content">
+						<span class="color_11">CONTACT:&nbsp;<Br> 00 212 5 23 37 93 01  &nbsp; <br> Contacter Nous <a href="mailto:info@monsite.fr" target="_self">toumihicham19@gmail.com</a>   </span><br><hr><span class="color_13">ADRESSE  <p>      </p> </span  <span class="color_13"><span style="font-weight:bold;">Route nationale N&deg;1 El haouziya</span>  <hr>HORAIRES D'OUVERTURE</span> <br> <span class="color_11">Lun-Ven&nbsp;: 10h - 17h    </span>   
+					</div>
+
+					<ul class="navbar-nav ml-auto"><li class="nav-item"><a href="../index.html" rel="" class="nav-link">Accueil</a></li><li class="nav-item active"><a href="infomerNous.php" rel="" class="nav-link">informer Nous !</a></li></ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript" src="../rw_common/themes/Artful/js/main.js"></script>
+</body>
+
+</html>
